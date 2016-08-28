0784268354b010ae2fe7b9d60dc6b89e3d7a8012
MrHKkk <ken121111@163.com>
2016-08-28 16:04:05 +0800

    spring integration test

---
+++ src/main/java/com/kd/test/si/messageflow/aggregator/MarketFiedServiceActivator.java
@@ -0,0 +1,27 @@
+package com.kd.test.si.messageflow.aggregator;
+
+import java.text.MessageFormat;
+import java.util.Map;
+
+import org.springframework.integration.annotation.Headers;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.stereotype.Component;
+
+import com.kd.test.messageflow.domain.Field;
+
+@Component
+public class MarketFiedServiceActivator {
+
+	@ServiceActivator
+	public Field handleField(Field dataField, @Headers Map<String, Object> headerMap) {
+		 
+		System.out.println(MessageFormat.format("{0}:{1}", dataField.getFieldDescriptor().toString(),
+				dataField.getValue()));
+		
+		for(String key : headerMap.keySet()) {
+			Object value = headerMap.get(key);
+			System.out.println(MessageFormat.format("header {0}:{1}", key, value));
+		}
+		return dataField;
+	}
+}
